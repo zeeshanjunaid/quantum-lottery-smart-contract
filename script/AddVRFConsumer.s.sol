9c25031c6f81601e7c4a4b7ed9d1e580ab5743be
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2025-08-18 08:13:31 +0500

    chore: add helper script to add VRF consumer

---
+++ script/AddVRFConsumer.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Interface} from "chainlink-brownie-contracts/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract AddVRFConsumer is Script {
+    function run() external {
+        address coordinator = vm.envAddress("VRF_COORDINATOR");
+        uint64 subId = uint64(vm.envUint("VRF_SUBSCRIPTION_ID"));
+        address consumer = vm.envAddress("CONSUMER");
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Interface(coordinator).addConsumer(subId, consumer);
+        vm.stopBroadcast();
+    }
+}
